e72e3f33caf5e1f48f2c7d6d7c2182f2a9d1698f
UULi <denny99203@gmail.com>
2024-10-15 21:42:22 +0800

    refactor: 整理 to_csv.py

--- pagereplacement/to_csv.py
+++ pagereplacement/to_csv.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from tqdm import *
 
+
 class ToCSV:
     def __init__(self, data):
         self.data = data
@@ -9,4 +10,3 @@ class ToCSV:
         combined_df = pd.concat(self.data, axis=1, keys=col_name)
         combined_df.to_csv(f"{path}/{filename}")
         tqdm.write(f"CSV file saved to {path}/{filename}")
-        
